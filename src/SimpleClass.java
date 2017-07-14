fdc4856bcd0e88435a44a16dcf228db219082d1b
Borbuev Beksultan <beksultan.borbuev@zensoft.io>
2017-07-14 12:00:32 +0600

    added new file(class)

---
+++ src/SimpleClass.java
@@ -0,0 +1,16 @@
+public class SimpleClass {
+    private int field_a;
+
+    public SimpleClass(int field_a) {
+        this.field_a = field_a;
+    }
+
+    public int getField_a() {
+
+        return field_a;
+    }
+
+    public void setField_a(int field_a) {
+        this.field_a = field_a;
+    }
+}
